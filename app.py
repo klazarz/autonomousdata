16dc3c56b02d5e5d447a8d0923c03fc36d550a9b
Kevin Lazarz <kevin.lazarz@oracle.com>
2021-10-08 22:59:53 +0200

    changes v1

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import socket
 from bs4 import BeautifulSoup
 import requests
+import login
 
-url ='https://read.amazon.com/kp/notebook'
+url ='https://www.dfb.de/3-liga/spieltagtabelle/?no_cache=1&spieledb_path=%2Fde%2Fcompetitions%2F3-liga%2Fseasons%2F2021-22%2Fmatchday%2Fcurrent'
 
-html_content = requests.get(url).text
-
-soup = BeautifulSoup(html_content, "lxml")
-
+r = requests.get(url)
+soup = BeautifulSoup(r.content, "lxml")
 
 print(soup.prettify())
